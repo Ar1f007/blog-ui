95031de79d2c8650ee4d150042da4c00382e5f98
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-03-27 15:14:31 +0600

    sidebar for dashbaord styled

--- src/features/dashboard/sidebar.tsx
+++ src/features/dashboard/sidebar.tsx
@@ -1,6 +1,8 @@
-import { List, ListItem, ListItemText, Paper } from '@mui/material';
+import { Box, List, ListItem, Paper, Stack, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 import paths from '../../routes/paths';
+import { createSXCollection } from '../../utils';
 
 const dashboardNavigation = [
   {
@@ -29,25 +31,47 @@ const dashboardNavigation = [
   },
 ];
 
+const styles = createSXCollection({
+  linkContainer: {
+    textDecoration: 'none',
+    color: 'inherit',
+    width: '100%',
+    px: 2,
+    py: 1,
+    borderRadius: 1,
+
+    '&:hover': {
+      backgroundColor: (t) => t.palette.grey['200'],
+    },
+  },
+  linkWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  link: {
+    fontWeight: 500,
+  },
+});
+
 export const Sidebar = () => (
   <Paper
     component="aside"
     elevation={4}
-    sx={{ p: 3 }}
+    sx={{ py: 2 }}
   >
     <List>
       {dashboardNavigation.map((link, index) => (
         <ListItem key={index}>
-          <ListItemText
-            primary={link.name}
-            secondary={10000}
-            sx={{
-              display: 'flex',
-              alignItems: 'flex-start',
-              justifyContent: 'space-between',
-            }}
-          />
-          {/* <Link to={link.path}>{link.name}</Link> */}
+          <Box
+            component={Link}
+            to={link.path}
+            sx={styles.linkContainer}
+          >
+            <Stack sx={styles.linkWrapper}>
+              <Typography sx={styles.link}>{link.name}</Typography>
+              <Typography>1000</Typography>
+            </Stack>
+          </Box>
         </ListItem>
       ))}
     </List>
